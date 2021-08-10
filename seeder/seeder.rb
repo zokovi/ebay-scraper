a1697f4b5ad363c146e8bd9fcbb0c5499369912e
ibnuthoriqh <ibnuthoriqh@gmail.com>
2021-08-11 01:17:02 +0700

    updated files

--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -1,5 +1,5 @@
 pages << {
-    url: 'https://www.ebay.com/sch/i.html?_from=R40&_trksid=p2334524.m570.l1313&_nkw=prusa+mk3s&_sacat=0&LH_TitleDesc=0&_odkw=prusa+mk3&_osacat=0',
-    page_type: 'listings',
-    method: "GET",
-  }
+  page_type: 'listings',
+  method: "GET",
+  url: "https://www.ebay.com/b/Apple-Cell-Phones-Smartphones/9355/bn_319682"
+}

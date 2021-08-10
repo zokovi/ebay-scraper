da6092d5eec9995934a30fbdd7fbdc1c7b8020ab
ibnuthoriqh <ibnuthoriqh@gmail.com>
2021-08-11 03:15:23 +0700

    Fix typo in listing scraper

--- parsers/listings.rb
+++ parsers/listings.rb
@@ -4,17 +4,18 @@ listings = nokogiri.css('ul.b-list__items_nofooter > li.s-item')
 
 listings.each do |listing|
     product = {}
-    product['title'] = listing.at_css('h3.s-item__title')&.text
+    product['title'] = item.at_css('h3.s-item__title')&.text
 
-    product['price'] = listing.at_css('span.s-item__price')&.text
+    product['price'] = item.at_css('span.s-item__price')&.text
+    
+    item_link = item.at_css('a.s-item__link')
 
-    item_link = listings.at_css('a.s-item__link')
     product['url'] = item_link['href'] unless item_link.nil?
 
     product['_collection'] = 'listings'
 
     outputs << product
-    
+
     pages << {
         url: product['url'],
         page_type: 'details',

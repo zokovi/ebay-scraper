1a946cb459c7260c0dcdc57df9aff5bfdc0893de
ibnuthoriqh <ibnuthoriqh@gmail.com>
2021-08-11 02:00:40 +0700

    created listing parser and updated config

---
+++ parsers/listings.rb
@@ -0,0 +1,17 @@
+nokogiri = Nokogiri.HTML(content)
+
+listings = nokogiri.css('ul.b-list__items_nofooter > li.s-item')
+
+listings.each do |listing|
+    product = {}
+    product['title'] = listing.at_css('h3.s-item__title')&.text
+
+    product['price'] = listing.at_css('span.s-item__price')&.text
+
+    item_link = listings.at_css('a.s-item__link')
+    product['url'] = item_link['href'] unless item_link.nil?
+
+    product['_collection'] = 'listings'
+
+    outputs << product
+end

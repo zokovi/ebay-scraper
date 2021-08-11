c243186ba6b88fd087c4508f54e73009d2d9af50
ibnuthoriqh <ibnuthoriqh@gmail.com>
2021-08-12 00:17:04 +0700

    added qa

--- finisher/finisher.rb
+++ finisher/finisher.rb
@@ -1,3 +1,5 @@
+require 'dh_easy/qa'
+
 collections = Datahen::Client::ScraperJobOutput.new.collections("ebay")
 collection = collections.find{ |collection| collection['collection'] == "listings" }
 if collection
@@ -13,3 +15,6 @@ else
    puts "no listings collection found"
 
 end
+
+vars = { "scraper_name" => "ebay", "collections" => ["listings"]}
+DhEasy::Qa::Validator.new.validate_internal(vars, outputs)
